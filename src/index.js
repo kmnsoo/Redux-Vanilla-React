86c2d408bdb1916e6d3b2279e068f7d301021d4f
kmnsoo <wkdska22@naver.com>
2021-10-29 15:00:40 +0900

    Add reducer(modifier state), store(storage) from redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
+import {createStore} from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("num");
 
-let count = 0;
 
-number.innerText = count;
 
-const updateText = () => {
-  number.innerText =count;
+//reducer는 함수여야한다. data를 modify한다. ex) add, minus
+const reducer = (state = 0) => {
+  return state;
 };
 
-const handleAdd = () => {
-  count = count +1 ; 
-  updateText();
-};
 
-const handleMinus = () => {
-  count = count -1; 
-  updateText();
-};
+//data가 저장되는 곳 -> store
+const store = createStore(reducer);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
