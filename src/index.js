f2917f6dbf23cd42c87ba28b2bc2893194f3cfe8
kmnsoo <wkdska22@naver.com>
2021-10-29 15:18:13 +0900

    Add dispatch for calling action

--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ const number = document.getElementById("num");
 
 
 
-//reducer는 함수여야한다. data를 modify한다. ex) add, minus
-const reducer = (state = 0) => {
-  return state;
+//reducer는 data를 modify한다. reducer는 함수여야한다.  ex) add, minus
+const reducer = (count = 0, action) => {
+  if (action.type ==="ADD"){
+    return count +1 ;
+  } else if(action.type ==="MINUS"){
+    return count -1;
+  } else{
+  return count;
+  }
 };
 
 
 //data가 저장되는 곳 -> store
 const store = createStore(reducer);
+
+//dispatch를 통해 action을 호출하고, dispatch 내부 인자는 object여만 한다.
+store.dispatch({ type: "ADD"} );
+store.dispatch({ type: "ADD"} );
+store.dispatch({ type: "ADD"} );
+store.dispatch({ type: "MINUS"} );
+
+console.log(store.getState());
 
